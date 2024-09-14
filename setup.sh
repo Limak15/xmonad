60156f50ce56e5af0022667916573e88e08cf796
Kamil <kamilgolub1@gmail.com>
2024-09-14 12:12:07 +0200

    Added Feather font and modified setup.sh

--- setup.sh
+++ setup.sh
@@ -35,6 +35,7 @@ sudo pacman -S --needed --noconfirm ${dependencies[@]}
 #Make sure the .config folder exists
 [ ! -d ~/.config ] && mkdir -p ~/.config
 [ ! -d ~/.local/bin ] && mkdir -p ~/.local/bin
+[ ! -d ~/.config/xmonad ] && mkdir -p ~/.config/xmonad
 
 #Link config
 for dir_path in "$CURR_DIR/config"/*; do
@@ -44,6 +45,8 @@ done
 for script_path in "$CURR_DIR/scripts"/*; do
     ln -svf "$script_path" "$HOME/.local/bin/$(basename "$script_path")"
 done
+
+ln -svf "$CURR_DIR/xmonad.hs" "$HOME/.config/xmonad.hs"
 
 #Create dirs for fonts if they dont exist
 [ ! -d "$FONTS_DIR" ] && mkdir -p "$FONTS_DIR" 
@@ -57,6 +60,11 @@ else
     unzip "$tmp/Meslo.zip" -d "$FONTS_DIR/ttf/Meslo/"
 fi
 
+if ! fc-list | grep -q "Feather"; then
+    mkdir -p "$FONTS_DIR/ttf/Feather"
+    cp "$CURR_DIR/fonts/Feather.ttf" "$FONTS_DIR/ttf/Feather/"
+fi
+
 [ ! -d "$BACKGROUND_DIR" ] && mkdir -p "$BACKGROUND_DIR"
 
 curl -o "$BACKGROUND_DIR/wallpaper.jpg" "$BACKGROUND_LINK"
@@ -64,6 +72,9 @@ curl -o "$BACKGROUND_DIR/wallpaper.jpg" "$BACKGROUND_LINK"
 echo "feh --no-fehbg --bg-scale '$BACKGROUND_DIR/wallpaper.jpg'" > ~/.fehbg
 chmod 755 ~/.fehbg
 echo "~/.fehbg &" > ~/.xsession
+
+network_interface=$(ip -o link show | awk '$9 == "UP" {print $2}' | sed 's/://')
+sed -i 's/interface = .*/interface ='"$network_interface"'/' $CURR_DIR/config/polybar/config.ini
 
 current_display_manager=$(systemctl show display-manager.service --property=Id --value)
 sudo systemctl disable $current_display_manager
